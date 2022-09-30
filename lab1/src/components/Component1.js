b16ec504ed0fd14a40667d9fd07b461b16d68e6c
allamatti98 <dayanallamatti98@gmail.com>
2022-09-30 12:55:42 +0300

    comp2

---
+++ lab1/src/components/Component1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const Comp1 = () => (
+    <div>
+        <h1>Wazzaa!!!</h1>
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+        <ol>
+            <li>Miami</li>
+            <li>Los Angeles</li>
+            <li>London</li>
+        </ol>
+    </div>
+)
+export default Comp1
---
+++ lab1/src/components/Component2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SomeContent(){
+    return (
+        <div>
+                    <h1>Function</h1>
+        <i>Hello</i>
+
+        </div>
+        )
+}
+export default SomeContent
