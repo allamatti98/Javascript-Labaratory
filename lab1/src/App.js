6b71714c7235735632a1a2298f4d509792a2e928
allamatti98 <dayanallamatti98@gmail.com>
2022-09-30 13:45:17 +0300

    Facts1

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Comp1 from './components/Component1';
 import SomeContent from './components/Component2';
 import NavBar from './components/NavBar';
+import ReactFacts from './components/Facts';
 
 function App() {
   return (
     <div id = "root">
-      <NavBar/>
-      <Comp1/>
-      <SomeContent/>
+      <img src = {logo} style = {{width: "9em"}}/>
+      <ReactFacts/>
     </div>
    
   );
---
+++ lab1/src/components/Facts.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function ReactFacts(){
+    return(
+        <div style={{fontSize: "2em", padding: "0px 100px"}}>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>It's Declarative</li>
+                <li>It's Declarative</li>
+                <li>It's Declarative</li>
+                <li>It's Declarative</li>
+                <li>It's Declarative</li>
+                <li>It's Declarative</li>
+            </ul>
+        </div>
+    )
+}
+export default ReactFacts
