c096b3c53ceba2a95bef73f3bf13c7a808b027af
allamatti98 <dayanallamatti98@gmail.com>
2022-09-30 13:19:36 +0300

    NavBar

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Comp1 from './components/Component1';
 import SomeContent from './components/Component2';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div id = "root">
+      <NavBar/>
       <Comp1/>
       <SomeContent/>
     </div>
---
+++ lab1/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <nav>
+            <h1>Lady Bird Boutique</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+export default NavBar
